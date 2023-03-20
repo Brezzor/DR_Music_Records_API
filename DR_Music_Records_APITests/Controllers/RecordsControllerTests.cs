6e730c7547545fe3811759250b15c8388d9a117b
Brezzor <oliv095w@edu.zealand.dk>
2023-03-20 14:32:08 +0100

    Initial setup.

---
+++ DR_Music_Records_APITests/Controllers/RecordsControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DR_Music_Records_API.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DR_Music_Records_API.Controllers.Tests
+{
+    [TestClass()]
+    public class RecordsControllerTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void GetTest1()
+        {
+
+        }
+
+        [TestMethod()]
+        public void PostTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void PutTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+
+        }
+    }
+}
