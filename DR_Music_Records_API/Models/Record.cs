ac57cc8350a3403c50524b1248bd04152212d8fe
Brezzor <oliv095w@edu.zealand.dk>
2023-03-20 14:32:08 +0100

    Initial setup.

---
+++ DR_Music_Records_API/Models/Record.cs
@@ -0,0 +1,11 @@
+﻿namespace DR_Music_Records_API.Models
+{
+    public class Record
+    {
+        public int Id { get; set; }
+        public string? title { get; set; }
+        public string? artist { get; set; }
+        public int duration { get; set; }
+        public int publicationYear { get; set; }
+    }
+}
---
+++ DR_Music_Records_API/Repositories/RecordRepository.cs
@@ -0,0 +1,24 @@
+﻿using DR_Music_Records_API.Models;
+
+namespace DR_Music_Records_API.Repositories
+{
+    public class RecordRepository
+    {
+        private int _nextId;
+        private List<Record> _records;
+
+        public RecordRepository() 
+        { 
+            _nextId = 0;
+            _records = new List<Record>()
+            {
+                new Record() { Id = _nextId++, title = "Thriller", artist = "Micheal Jackson", duration = 42, publicationYear = 1982},
+                new Record() { Id = _nextId++, title = "Back in Black", artist = "AC/DC", duration = 42, publicationYear = 1980},
+                new Record() { Id = _nextId++, title = "The Bodyguard", artist = "Whitney Houston", duration = 57, publicationYear = 1992},
+                new Record() { Id = _nextId++, title = "The Dark Side of the Moon", artist = "Pink Floyd", duration = 42, publicationYear = 1973},
+                new Record() { Id = _nextId++, title = "Hotel California", artist = "Eagles", duration = 43, publicationYear = 1976},
+                new Record() { Id = _nextId++, title = "21", artist = "Adele", duration = 48, publicationYear = 2010}
+            }
+        }
+    }
+}
